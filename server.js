aa6c3984b1168df205d4d81d1b6648010f5c1b0b
gupteg <gupteg@outlook.com>
2025-10-23 22:12:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -338,4 +338,5 @@ io.on('connection', (socket) => {
 // --- END: Retained Logic ---
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+server.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
